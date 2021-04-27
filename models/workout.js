19e6d2dfde6d031f2de22e508b1023e64107dc03
B.A. Huisken <brice.huisken@gmail.com>
2021-04-26 23:52:50 -0600

    what I have so far

---
+++ models/workout.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      name: {
+        type: String
+      }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
