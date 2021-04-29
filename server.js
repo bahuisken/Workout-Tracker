e03246e6fc573cffab6cf96a2059e7c919c80c1c
B.A. Huisken <brice.huisken@gmail.com>
2021-04-28 23:09:13 -0600

    fixed htmlRoutes

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
-app.use(require("./routes/htmlroutes"));
+app.use(require("./routes/htmlRoutes"));
 app.use(require("./routes/apiRoutes"));
 
 app.listen(PORT, () => {
